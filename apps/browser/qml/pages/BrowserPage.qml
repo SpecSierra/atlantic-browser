sailfishos/sailfish-browser

commit ff92005de4c5cc828e037e98badbe8fb14c2155c

[browser] Support touch events when selection active. Fixes JB#56311

apps/browser/qml/pages/BrowserPage.qml
@@ -36,6 +36,7 @@ Page {
     property alias url: webView.url
     property alias title: webView.title
     property alias webView: webView
+    property alias inputRegion: inputRegion
 
     function load(url, title) {
         webView.load(url, title)
@@ -64,7 +65,7 @@ Page {
         var mask = Qt.rect(0, 0,
                            portraitScreen ? Screen.width : Screen.height,
                            portraitScreen ? Screen.height : Screen.width)
-        if (webView.enabled && browserPage.active && !webView.touchBlocked && !downloadPopup.visible) {
+        if (webView.enabled && browserPage.active && !downloadPopup.visible) {
             var overlayVisibleHeight = browserPage.height - overlay.y
 
             switch (window.QuickWindow.Screen.angleBetween(orientation, window.QuickWindow.Screen.primaryOrientation)) {
@@ -222,6 +223,8 @@ Page {
     }
 
     InputRegion {
+        id: inputRegion
+
         window: webView.chromeWindow
         x: inputMask.x
         y: inputMask.y

apps/core/inputregion.cpp
@@ -23,6 +23,8 @@ InputRegionPrivate::InputRegionPrivate(InputRegion *q)
     , y(0.0)
     , width(0.0)
     , height(0.0)
+    , selectionStartHandleMask(0.0, 0.0, 0.0, 0.0)
+    , selectionEndHandleMask(0.0, 0.0, 0.0, 0.0)
     , window(0)
     , q_ptr(q)
     , updateTimerId(0)
@@ -36,7 +38,12 @@ void InputRegionPrivate::update()
     updateTimerId = 0;
 
     if (window) {
-        QRegion mask(x, y, width, height);
+        QRect rects[3];
+        rects[0] = selectionStartHandleMask;
+        rects[1] = selectionEndHandleMask;
+        rects[2].setRect(x, y, width, height);
+        QRegion mask;
+        mask.setRects(rects, 3);
         window->setMask(mask);
         if (window->handle()) {
             QPlatformNativeInterface *native = QGuiApplication::platformNativeInterface();
@@ -124,6 +131,38 @@ void InputRegion::setHeight(qreal height)
         d->height = height;
         d->scheduleUpdate();
         emit heightChanged();
+    }
+}
+
+const QRect& InputRegion::selectionStartHandleMask() const
+{
+    Q_D(const InputRegion);
+    return d->selectionStartHandleMask;
+}
+
+void InputRegion::setSelectionStartHandleMask(const QRect& rect)
+{
+    Q_D(InputRegion);
+    if (d->selectionStartHandleMask != rect) {
+        d->selectionStartHandleMask = rect;
+        d->scheduleUpdate();
+        emit selectionStartHandleMaskChanged();
+    }
+}
+
+const QRect& InputRegion::selectionEndHandleMask() const
+{
+    Q_D(const InputRegion);
+    return d->selectionEndHandleMask;
+}
+
+void InputRegion::setSelectionEndHandleMask(const QRect& rect)
+{
+    Q_D(InputRegion);
+    if (d->selectionEndHandleMask != rect) {
+        d->selectionEndHandleMask = rect;
+        d->scheduleUpdate();
+        emit selectionEndHandleMaskChanged();
     }
 }
 

apps/core/inputregion.h
@@ -13,6 +13,7 @@
 #define INPUTREGION_H
 
 #include <QObject>
+#include <QRect>
 #include <qqml.h>
 
 class QWindow;
@@ -26,6 +27,8 @@ class InputRegion : public QObject
     Q_PROPERTY(qreal y READ y WRITE setY NOTIFY yChanged FINAL)
     Q_PROPERTY(qreal width READ width WRITE setWidth NOTIFY widthChanged FINAL)
     Q_PROPERTY(qreal height READ height WRITE setHeight NOTIFY heightChanged FINAL)
+    Q_PROPERTY(QRect selectionStartHandleMask READ selectionStartHandleMask WRITE setSelectionStartHandleMask NOTIFY selectionStartHandleMaskChanged FINAL)
+    Q_PROPERTY(QRect selectionEndHandleMask READ selectionEndHandleMask WRITE setSelectionEndHandleMask NOTIFY selectionEndHandleMaskChanged FINAL)
     Q_PROPERTY(QWindow *window READ window WRITE setWindow NOTIFY windowChanged FINAL)
 public:
     InputRegion(QObject *parent = 0);
@@ -42,6 +45,11 @@ public:
     qreal height() const;
     void setHeight(qreal height);
 
+    const QRect& selectionStartHandleMask() const;
+    void setSelectionStartHandleMask(const QRect& rect);
+    const QRect& selectionEndHandleMask() const;
+    void setSelectionEndHandleMask(const QRect& rect);
+
     QWindow *window() const;
     void setWindow(QWindow *window);
 
@@ -50,6 +58,8 @@ signals:
     void yChanged();
     void widthChanged();
     void heightChanged();
+    void selectionStartHandleMaskChanged();
+    void selectionEndHandleMaskChanged();
     void windowChanged();
 
 protected:

apps/core/declarativewebcontainer.cpp
@@ -59,6 +59,7 @@ DeclarativeWebContainer::DeclarativeWebContainer(QWindow *parent)
     , m_foreground(true)
     , m_allowHiding(true)
     , m_touchBlocked(false)
+    , m_selectionActive(false)
     , m_portrait(true)
     , m_fullScreenMode(false)
     , m_fullScreenHeight(0.0)
@@ -799,7 +800,7 @@ void DeclarativeWebContainer::touchEvent(QTouchEvent *event)
         return;
     }
 
-    if (m_webPage && m_enabled && !m_touchBlocked) {
+    if (m_webPage && m_enabled && ((!m_touchBlocked || m_selectionActive) || event->type() != QEvent::TouchBegin)) {
         QList<QTouchEvent::TouchPoint> touchPoints = event->touchPoints();
         QTouchEvent mappedTouchEvent = *event;
 
@@ -810,6 +811,12 @@ void DeclarativeWebContainer::touchEvent(QTouchEvent *event)
 
         mappedTouchEvent.setTouchPoints(touchPoints);
         m_webPage->touchEvent(&mappedTouchEvent);
+        if (m_selectionActive && event->type() == QEvent::TouchBegin) {
+            // If touch event is received here after selection is active it's pressed outside selection area
+            // Inform that selection should close
+            m_selectionActive = false;
+            emit selectionActiveChanged();
+        }
     } else {
         QWindow::touchEvent(event);
     }

apps/core/declarativewebcontainer.h
@@ -47,6 +47,7 @@ class DeclarativeWebContainer : public QWindow, public QQmlParserStatus, protect
     Q_PROPERTY(int maxLiveTabCount READ maxLiveTabCount WRITE setMaxLiveTabCount NOTIFY maxLiveTabCountChanged FINAL)
     // This property should cover all possible popus
     Q_PROPERTY(bool touchBlocked MEMBER m_touchBlocked NOTIFY touchBlockedChanged FINAL)
+    Q_PROPERTY(bool selectionActive MEMBER m_selectionActive NOTIFY selectionActiveChanged FINAL)
     Q_PROPERTY(bool portrait READ portrait WRITE setPortrait NOTIFY portraitChanged FINAL)
     Q_PROPERTY(bool fullscreenMode MEMBER m_fullScreenMode NOTIFY fullscreenModeChanged FINAL)
     Q_PROPERTY(qreal fullscreenHeight MEMBER m_fullScreenHeight NOTIFY fullscreenHeightChanged FINAL)
@@ -173,6 +174,7 @@ signals:
     void allowHidingChanged();
     void maxLiveTabCountChanged();
     void touchBlockedChanged();
+    void selectionActiveChanged();
     void portraitChanged();
     void fullscreenModeChanged();
     void fullscreenHeightChanged();
@@ -277,6 +279,7 @@ private:
     bool m_foreground;
     bool m_allowHiding;
     bool m_touchBlocked;
+    bool m_selectionActive;
     bool m_portrait;
     bool m_fullScreenMode;
     qreal m_fullScreenHeight;

apps/core/inputregion_p.h
@@ -14,6 +14,7 @@
 
 #include <QObject>
 #include <QWindow>
+#include <QRect>
 #include "inputregion.h"
 
 class InputRegionPrivate {
@@ -27,6 +28,8 @@ public:
     qreal y;
     qreal width;
     qreal height;
+    QRect selectionStartHandleMask;
+    QRect selectionEndHandleMask;
     QWindow *window;
     InputRegion *q_ptr;
     int updateTimerId;

apps/shared/WebView.qml
@@ -55,6 +55,9 @@ WebContainer {
             }
 
             Behavior on opacity { FadeAnimator {} }
+
+            onStartHandleMaskChanged: browserPage.inputRegion.selectionStartHandleMask = startHandleMask
+            onEndHandleMaskChanged: browserPage.inputRegion.selectionEndHandleMask = endHandleMask
         }
     }
 
@@ -106,9 +109,18 @@ WebContainer {
     fullscreenMode: (contentItem && !contentItem.chrome) ||
                     (contentItem && contentItem.fullscreen)
 
+    selectionActive: webView.contentItem && webView.contentItem.textSelectionActive
     touchBlocked: contentItem && contentItem.popupOpener && contentItem.popupOpener.active ||
-                  webView.contentItem && webView.contentItem.textSelectionActive || !AccessPolicy.browserEnabled || false
+                   selectionActive || !AccessPolicy.browserEnabled || false
     favicon: contentItem ? contentItem.favicon : ""
+
+    onSelectionActiveChanged: {
+        if (!selectionActive && webView.contentItem && webView.contentItem.textSelectionController) {
+            webView.contentItem.textSelectionController.clearSelection()
+            browserPage.inputRegion.selectionStartHandleMask = Qt.rect(0, 0, 0, 0)
+            browserPage.inputRegion.selectionEndHandleMask = Qt.rect(0, 0, 0, 0)
+        }
+    }
 
     webPageComponent: Component {
         WebPage {
@@ -168,8 +180,10 @@ WebContainer {
             }
 
             function clearSelection() {
-                if (textSelectionActive) {
+                if (textSelectionController) {
                     textSelectionController.clearSelection()
+                    browserPage.inputRegion.selectionStartHandleMask = Qt.rect(0, 0, 0, 0)
+                    browserPage.inputRegion.selectionEndHandleMask = Qt.rect(0, 0, 0, 0)
                 }
             }
 

rpm/sailfish-browser.spec
@@ -1,7 +1,7 @@
 %global min_xulrunner_version 45.8.1.1
 %global min_qtmozembed_version 1.53.8
 %global min_embedlite_components_version 1.20.0
-%global min_sailfishwebengine_version 1.3.0
+%global min_sailfishwebengine_version 1.5.9
 %global min_systemsettings_version 0.5.25
 
 %global captiveportal sailfish-captiveportal

SpecSierra/atlantic-browser

commit 9e8c4efc54d401e9ff85e43662a646c69b1223f0

[browser] Fix text selection handle grabbing on landscape. JB#63243
The handle coordinates weren't properly translated to display coordinates
on the InputRegion. Thus it wasn't possible to grab them.

Made now the translation responsibility of the InputRegion to get it
done in one place instead of each region source separately.

Bug was regression from commit ff92005de4c5cc.

apps/browser/qml/pages/BrowserPage.qml
@@ -61,36 +61,6 @@ Page {
         window.activate()
     }
 
-    function inputMaskForOrientation(orientation) {
-        // mask is in portrait window coordinates
-        var portraitScreen = window.QuickWindow.Screen.primaryOrientation === Qt.PortraitOrientation
-        var mask = Qt.rect(0, 0,
-                           portraitScreen ? Screen.width : Screen.height,
-                           portraitScreen ? Screen.height : Screen.width)
-        if (webView.enabled && browserPage.active && !webView.touchBlocked && !downloadPopup.visible) {
-            var overlayVisibleHeight = browserPage.height - overlay.y
-
-            switch (window.QuickWindow.Screen.angleBetween(orientation, window.QuickWindow.Screen.primaryOrientation)) {
-            case 0:
-            case 360:
-                mask.y = overlay.y
-                // fallthrough
-            case 180:
-            case -180:
-                mask.height = overlayVisibleHeight
-                break
-            case 270:
-            case -90:
-                mask.x = overlay.y
-                // fallthrough
-            case 90:
-            case -270:
-                mask.width = overlayVisibleHeight
-            }
-        }
-        return mask
-    }
-
     // for time being make this fullscreen. TODO: avoid drawing over cutout and corner areas.
     cutoutMode: CutoutMode.FullScreen
     background: null
@@ -242,7 +212,10 @@ Page {
         id: inputRegion
 
         window: webView.chromeWindow
-        overlayMask: inputMaskForOrientation(orientation)
+        orientation: browserPage.orientation // Qt and Silica orientations match
+        overlayMask: (webView.enabled && browserPage.active && !webView.touchBlocked && !downloadPopup.visible)
+                     ? Qt.rect(0, overlay.y, browserPage.width, browserPage.height - overlay.y)
+                     : Qt.rect(0, 0, browserPage.width, browserPage.height)
     }
 
     Browser.DimmerEffect {

apps/captiveportal/qml/pages/CaptivePortalPage.qml
@@ -41,36 +41,6 @@ Page {
         }
     }
 
-    function inputMaskForOrientation(orientation) {
-        // mask is in portrait window coordinates
-        var portraitScreen = window.QuickWindow.Screen.primaryOrientation === Qt.PortraitOrientation
-        var mask = Qt.rect(0, 0,
-                           portraitScreen ? Screen.width : Screen.height,
-                           portraitScreen ? Screen.height : Screen.width)
-        if (webView.enabled && browserPage.active && !webView.touchBlocked) {
-            var overlayVisibleHeight = browserPage.height - overlay.y
-
-            switch (window.QuickWindow.Screen.angleBetween(orientation, window.QuickWindow.Screen.primaryOrientation)) {
-            case 0:
-            case 360:
-                mask.y = overlay.y
-                // fallthrough
-            case 180:
-            case -180:
-                mask.height = overlayVisibleHeight
-                break
-            case 270:
-            case -90:
-                mask.x = overlay.y
-                // fallthrough
-            case 90:
-            case -270:
-                mask.width = overlayVisibleHeight
-            }
-        }
-        return mask
-    }
-
     property int pageOrientation: pageStack.currentPage._windowOrientation
     onPageOrientationChanged: {
         // When on other pages update immediately.
@@ -169,7 +139,10 @@ Page {
 
     InputRegion {
         window: webView.chromeWindow
-        overlayMask: inputMaskForOrientation(orientation)
+        orientation: browserPage.orientation // Qt and Silica orientations match
+        overlayMask: (webView.enabled && browserPage.active && !webView.touchBlocked)
+                     ? Qt.rect(0, overlay.y, browserPage.width, browserPage.height - overlay.y)
+                     : Qt.rect(0, 0, browserPage.width, browserPage.height)
     }
 
     Browser.CaptivePortalOverlay {

apps/core/inputregion.cpp
@@ -14,6 +14,8 @@
 #include <QGuiApplication>
 #include <QRegion>
 #include <QWindow>
+#include <QScreen>
+#include <QDebug>
 
 #include <qpa/qplatformnativeinterface.h>
 
@@ -25,6 +27,7 @@ class InputRegionPrivate
 public:
     InputRegionPrivate(InputRegion *q);
 
+    QRect translateRect(QRect input);
     void scheduleUpdate();
     void update();
 
@@ -34,6 +37,7 @@ public:
     QWindow *window;
     InputRegion *q_ptr;
     int updateTimerId;
+    Qt::ScreenOrientation orientation;
 
     Q_DECLARE_PUBLIC(InputRegion)
 };
@@ -45,7 +49,37 @@ InputRegionPrivate::InputRegionPrivate(InputRegion *q)
     , window(0)
     , q_ptr(q)
     , updateTimerId(0)
+    , orientation(Qt::PortraitOrientation)
 {
+}
+
+QRect InputRegionPrivate::translateRect(QRect input)
+{
+    QRect result = input;
+    QScreen *screen = qApp->primaryScreen();
+    if (!screen) {
+        return result;
+    }
+
+    int angle = screen->angleBetween(orientation, screen->primaryOrientation());
+
+    if (angle == 180 || angle == -180) {
+        result.moveTop(screen->size().height() - input.y() - input.height());
+        result.moveLeft(screen->size().width() - input.x() - input.width());
+    } else if (qAbs(angle) == 270 || qAbs(angle) == 90) {
+        result.setWidth(input.height());
+        result.setHeight(input.width());
+    }
+
+    if (angle == 270 || angle == -90) {
+        result.moveLeft(input.y());
+        result.moveTop(screen->size().height() - input.width() - input.x());
+    } else if (angle == -270 || angle == 90) {
+        result.moveTop(input.x());
+        result.moveLeft(screen->size().width() - input.height() - input.y());
+    }
+
+    return result;
 }
 
 void InputRegionPrivate::update()
@@ -56,9 +90,9 @@ void InputRegionPrivate::update()
 
     if (window) {
         QRect rects[3];
-        rects[0] = selectionStartHandleMask;
-        rects[1] = selectionEndHandleMask;
-        rects[2] = overlayMask;
+        rects[0] = translateRect(selectionStartHandleMask);
+        rects[1] = translateRect(selectionEndHandleMask);
+        rects[2] = translateRect(overlayMask);
         QRegion mask;
         mask.setRects(rects, 3);
         window->setMask(mask);
@@ -148,6 +182,22 @@ void InputRegion::setWindow(QWindow *window)
         d->window = window;
         d->scheduleUpdate();
         emit windowChanged();
+    }
+}
+
+int InputRegion::orientation() const
+{
+    Q_D(const InputRegion);
+    return d->orientation;
+}
+
+void InputRegion::setOrientation(int orientation)
+{
+    Q_D(InputRegion);
+    if (d->orientation != orientation) {
+        d->orientation = static_cast<Qt::ScreenOrientation>(orientation);
+        d->scheduleUpdate();
+        emit orientationChanged();
     }
 }
 

apps/core/inputregion.h
@@ -26,6 +26,7 @@ class InputRegion : public QObject
     Q_PROPERTY(QRect selectionStartHandleMask READ selectionStartHandleMask WRITE setSelectionStartHandleMask NOTIFY selectionStartHandleMaskChanged FINAL)
     Q_PROPERTY(QRect selectionEndHandleMask READ selectionEndHandleMask WRITE setSelectionEndHandleMask NOTIFY selectionEndHandleMaskChanged FINAL)
     Q_PROPERTY(QWindow *window READ window WRITE setWindow NOTIFY windowChanged FINAL)
+    Q_PROPERTY(int orientation READ orientation WRITE setOrientation NOTIFY orientationChanged)
 
 public:
     InputRegion(QObject *parent = 0);
@@ -40,11 +41,15 @@ public:
     QWindow *window() const;
     void setWindow(QWindow *window);
 
+    int orientation() const;
+    void setOrientation(int orientation);
+
 signals:
     void overlayMaskChanged();
     void selectionStartHandleMaskChanged();
     void selectionEndHandleMaskChanged();
     void windowChanged();
+    void orientationChanged();
 
 protected:
     void timerEvent(QTimerEvent *);